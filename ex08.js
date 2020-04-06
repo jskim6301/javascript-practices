7dc82996b8474c33d157b790940ba758c365a606
jskim <jskim@naver.com>
2020-04-06 15:58:07 +0900

    init commit

---
+++ ex08.js
@@ -0,0 +1,32 @@
+var o = {
+	name:"둘리",
+	age:10,
+	isFemale:false
+};
+
+var f= function(){
+	console.log("Hello world");
+};
+console.log(o);
+
+console.log("========================");
+
+o.another = {
+		name:"마이콜",
+		age:30
+};
+f.another = {
+		name:"또치",
+		age:10,
+		info:function(){
+			console.log(this.name+":"+this.age);
+		}
+}
+console.log(o);
+console.log(f.another);
+console.log(f.another.info);
+
+console.log("==============기본타입은 확장불가=================");
+var i = 10;
+i.another = {};
+console.log(i.another);//undefined
